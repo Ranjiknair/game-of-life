77557b2daad4231c5d3eccf73e50dc95d1a8512f
Ranjiknair <36860053+Ranjiknair@users.noreply.github.com>
2020-04-12 16:53:27 +0530

    https://github.com/Ranjiknair/game-of-life.git
    https://github.com/Ranjiknair/game-of-life.git

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,7 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
- * new commit on github-testing ranji
+ * Removed the new commit
  */
 @Controller
 @RequestMapping("/game")
